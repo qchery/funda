36b9ba74a234c036b609700955938b5600a16561
Chery <chinrui1016@163.com>
2018-03-27 09:07:49 +0800

    优化 LogResponseBodyAdvice，去除Uri中的ContextPath

--- src/main/java/com/qchery/funda/advice/LogResponseBodyAdvice.java
+++ src/main/java/com/qchery/funda/advice/LogResponseBodyAdvice.java
@@ -7,8 +7,11 @@ import org.springframework.core.MethodParameter;
 import org.springframework.http.MediaType;
 import org.springframework.http.server.ServerHttpRequest;
 import org.springframework.http.server.ServerHttpResponse;
+import org.springframework.http.server.ServletServerHttpRequest;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice;
+
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * @author Chery
@@ -28,7 +31,9 @@ public class LogResponseBodyAdvice implements ResponseBodyAdvice {
     public Object beforeBodyWrite(Object body, MethodParameter returnType, MediaType selectedContentType,
                                   Class selectedConverterType, ServerHttpRequest request, ServerHttpResponse response) {
         if (logger.isDebugEnabled()) {
-            logger.debug("uri={} | responseBody={}", request.getURI().getPath(), JsonDesUtils.toLogString(body));
+            HttpServletRequest servletRequest = ((ServletServerHttpRequest) request).getServletRequest();
+            String requestUriWithoutContextPath = servletRequest.getRequestURI().substring(servletRequest.getContextPath().length());
+            logger.debug("uri={} | responseBody={}", requestUriWithoutContextPath, JsonDesUtils.toLogString(body));
         }
         return body;
     }
